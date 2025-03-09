389b5e4d85034dd5d18ef2dc77445a7bb160ed9f
M A Murad <72880355+mozaddedalfeshani@users.noreply.github.com>
2025-03-09 22:53:08 +0600

    add welcome screen and styling components; update app icon and splash image

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <Typo size={30} >Welcome</Typo>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { colors } from "@/constants/theme";
+import { useRouter } from "expo-router";
 
 const index = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/welcome");
+    }, 2000);
+  }, []);
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../assets/images/splashImage.png")}
+      />
     </View>
   );
 };
 
 export default index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    height: "20%",
+    aspectRatio: 1,
+  },
+});
